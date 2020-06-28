ff6972b803349b30e5a7f2a030d3ad55f5bf1263
IftikharAhmadPir <iftikharahmad.pir@gmail.com>
2020-06-28 16:26:12 -0700

    MyFirstCommit

---
+++ DBWTechniqueBackend/FunctionalityTest.cs
@@ -0,0 +1,27 @@
+﻿using DBWTechniqueData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBWTechniqueBackend
+{
+    class FunctionalityTest
+    {
+        ProxyData DataLayerObj = new ProxyData();
+        public void runAllTests()
+        {
+
+        }
+
+        public void getProxyFromDatabaseAndUpdateStatus()
+        {
+            bool CountCheck = DataLayerObj.GetProxyFromDatabase().Count > 1;
+            if(CountCheck == true)
+            {
+                DataLayerObj.setFunctionalityTestingResults("GetProxyFromDatabase", 1);
+            }
+        }
+    }
+}
---
+++ DBWTechniqueRESTApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.EnterpriseServices;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http.Headers;
+using System.Windows;
+using System.Net.Http.Formatting;
+using System.Web.Script.Serialization;
+using Microsoft.Ajax.Utilities;
+
+namespace DBWTechniqueRESTApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Tests()
+        {
+            return View();
+        }
+
+    }
+}
